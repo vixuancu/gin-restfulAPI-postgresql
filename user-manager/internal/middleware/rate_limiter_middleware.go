098d0af60ab1a14e97cf0148dcf9dd91ab76efdc
vixuancu <vixuancu2004@gmail.com>
2025-08-11 09:21:05 +0700

    tesst

--- user-manager/internal/middleware/rate_limiter_middleware.go
+++ user-manager/internal/middleware/rate_limiter_middleware.go
@@ -80,7 +80,7 @@ func RateLimitMiddleware(rateLimiterLogger *zerolog.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// dùng ip xác đinh người dùng
 		ip := getClientIP(c)
-		//log.Println("ip:", ip) //::1 <=> 127.0.0.1 tesst
+		//log.Println("ip:", ip) //::1 <=> 127.0.0.1 tesst sdasdasdn dasads
 		limiter := getRateLimitter(ip)
 		if !limiter.Allow() {
 			if shoudLogRateLimit(ip) {
